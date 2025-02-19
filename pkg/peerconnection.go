277be13c48012a92c15477cd54841bde49ed8f9f
harshabose <harshavardhanbose@gmail.com>
2025-02-19 23:46:13 +0530

    first commit

--- pkg/peerconnection.go
+++ pkg/peerconnection.go
@@ -1,8 +1,11 @@
 package pkg
 
-import "github.com/pion/webrtc/v4"
+import (
+	"github.com/harshabose/simple_webrtc_comm/datachannel/pkg"
+	"github.com/pion/webrtc/v4"
+)
 
 type PeerConnection struct {
 	peerConnection *webrtc.PeerConnection
-	dataChannels   []*webrtc.DataChannel
+	dataChannels   *data.DataChannels
 }
